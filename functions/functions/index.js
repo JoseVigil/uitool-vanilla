ca2f49e84c728b43076b56cee97597d4f7c53830
Jose Vigil <josemanuelvigil@gmail.com>
2020-07-11 13:32:54 -0300

    Creacion de imagen de html funciona pero no en cloud.
    Tengo que crear un cloud function independiente y asignarle recursos.
    
    Probando.

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -3,27 +3,28 @@
     const functions = require('firebase-functions');
     const firebase = require('firebase');
     var admin = require('firebase-admin');
+    //const chromium = require("chrome-aws-lambda");
     const puppeteer = require('puppeteer');
-    const nodeHtmlToImage = require('node-html-to-image');
+    //const puppeteer = require("puppeteer-core");
+    //const nodeHtmlToImage = require('node-html-to-image');
     const request = require('request');    
 
     const htmlFunctions = require('./html_functions.js');
 
     const app = express();
 
-    var _payloadUrl = "http://noti.ms/";
+    //var _payloadUrl = "http://noti.ms/";
+    var _payloadUrl = "http://notimation-ms.firebaseapp.com/";
 
-    // Runs before every route. Launches headless Chrome.
-    app.all('*', async (req, res, next) => {
+    /*app.all('*', async (req, res, next) => {
         // Note: --no-sandbox is required in this env.
         // Could also launch chrome and reuse the instance
         // using puppeteer.connect()
-        res.locals.browser = await puppeteer.launch({
-          args: ['--no-sandbox']
+        res.locals.browser = await puppeteer.launch({          
+          args: ['--no-sandbox', '--disable-setuid-sandbox'],
         });
         next(); // pass control to next route.
-    });
-    
+    });*/
 
     var serviceAccount = require("./key/notimation-ms-firebase-adminsdk-jbv0c-5463f12f44.json");
     var db_url = "https://notimation-ms.firebaseio.com";
@@ -34,8 +35,8 @@
         //credential: admin.credential.applicationDefault()
     });
     
-    const db = admin.firestore();
-    //const storage = admin.storage();
+    const db = admin.firestore();    
+    const storage = admin.storage();
 
     const firebaseConfig = {
         apiKey: "AIzaSyBtxWKuwsGfMoDZ_0yDvC7w0SEbEno418U",
@@ -48,24 +49,22 @@
         measurementId: "G-QW5PLEMZTN"
     };
     firebase.initializeApp(firebaseConfig);
-    firebase.functions().useFunctionsEmulator('http://localhost:5000'); 
-    
-    // // Create and Deploy Your First Cloud Functions
-    // // https://firebase.google.com/docs/functions/write-firebase-functions
+    firebase.functions().useFunctionsEmulator('http://localhost:5000');    
+  
+    /**
+     * Post SMS
+     */
 
-    exports.helloWorld = functions.https.onRequest((request, response) => {        
-      response.send("Hello from Firebaseeeee!");
-    }); 
-
-
-    exports.saveSMS = functions.https.onRequest( async  (req, res) => {        
+    exports.postSMS = functions.https.onRequest( async  (req, res) => {        
 
       const _phone = req.body.data.phone;
       const _name = req.body.data.name;
       const _code = req.body.data.code; 
       const _client_id = req.body.data.client_id;  
       const _message = req.body.data.message;
-      const _contact = req.body.data.contact;              
+      const _contact = req.body.data.contact;   
+      
+      let _time = admin.firestore.FieldValue.serverTimestamp(); 
       
       db.collection("cobranzas").add({
         phone: _phone,
@@ -73,28 +72,23 @@
         code: _code,
         client_id:_client_id,
         message: _message,
-        contact: _contact        
+        contact: _contact,
+        time_created: _time        
       })
       .then(function(docRef) {           
           
-        /*postSMS(req, docRef, function(result){          
+        postRequestSMS(req, docRef, function(result){
+          res.send(JSON.stringify(result));
+        });                 
+        return docRef.id;
 
-          res.send(JSON.stringify(result)); 
-
-        });*/         
-        
-        res.send(docRef.id); 
-
-      })        
-      .catch(function(error) {
-          console.error("Error adding document: ", error);
+      }).catch((error) => {     
+        console.error("Error adding document: ", error);           
       });   
 
-      //res.status(200).send(hola);
-      
     });
 
-    function postSMS(req, docRef, callback) {
+    function postRequestSMS(req, docRef, callback) {
 
       let _id = docRef.id; 
       const _phone = req.body.data.phone;        
@@ -123,58 +117,151 @@
               var smsid = _data["sms_id"];                         
               return docRef.update({
                 sms_id: smsid
-              })
-              .then(function() {
+              }).then(function() {                
                   callback(smsid);
-              })
-              .catch(function(e) {                
+                  return smsid;
+              }).catch(function(e) {                
                   callback(e);
               });                     
           } else {                
               var _error = JSON.parse(error); 
-              callback(_error);                
+              callback(_error);   
+              return _error;             
           }            
       });       
     } 
 
+    /**
+     * Convert Html To Image
+     */
+
     exports.convertHtmlToImage = functions.firestore
       .document('cobranzas/{cobranzasId}')
-      .onWrite( async (change, context) => {    
-
+      .onCreate( async (change, context) => {          
+      
+      //const newValue = change.after.data();       
+      //let name = newValue.name;
+      //let message = newValue.message;
+      //let cobranzasId = context.params.cobranzasId;   
+ 
       const newValue = change.after.data();       
       let name = newValue.name;
       let message = newValue.message;
       let cobranzasId = context.params.cobranzasId;   
 
-      let image64 = await nodeHtmlToImage({
-        html: htmlFunctions.buildHTMLForImage(name, message),
-        //content: [{ name : name, message : message }],
-        encoding: "base64" 
-      }); 
+      //console.log('name:', name);
+      //console.log('message:', message);
+      //console.log('cobranzasId:', cobranzasId);    
+
+      let _html = htmlFunctions.buildHTMLForImage(name, message).toString();
+
+      //console.log('_html:', _html);
+
+      /*let image64 = await nodeHtmlToImage({
+        html: _html,    
+        encoding: "base64",
+        puppeteerArgs : ['--no-sandbox', '--disable-setuid-sandbox']
+      }).catch((error) => {
+        console.log('Error nodeHtmlToImage:', error);
+        return error;
+      });*/
+    
+      var screenshotBuffer = null;
+      var browser = null;
       
-      await db.collection("cobranzas").doc(cobranzasId).update(
-        {image: image64}
-      );
+      try {
+
+        //https://www.ribice.ba/cloud-function-html-to-pdf/
+
+        const browser = await puppeteer.launch({          
+          headless: false,
+          args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        });
+
+        /*const browser = await puppeteer.launch({
+          args: chromium.args,
+          defaultViewport: chromium.defaultViewport,
+          executablePath: await chromium.executablePath,
+          headless: chromium.headless
+        });*/
+
+        /*browser = await chromium.puppeteer.launch({
+          args: chromium.args,
+          defaultViewport: chromium.defaultViewport,
+          executablePath: await chromium.executablePath,
+          headless: chromium.headless,
+          ignoreHTTPSErrors: true,
+        });*/
+
+        const page = await browser.newPage();      
+        await page.setViewport({
+                  width: 1200,
+                  height: 630,
+                  deviceScaleFactor: 1,
+                });
+        await page.setContent(_html);       
+        screenshotBuffer = await page.screenshot({encoding: "binary"});
+        await browser.close(); 
+
+      } catch (e) {
+        console.error(e);
+        return e;
+      }      
+
+      const bucket = admin.storage().bucket("notimation-ms.appspot.com");
+      const imageBuffer = new Uint8Array(screenshotBuffer);
+      const file = bucket.file(`cobranzas/sipef/${sms_id}.png`, {
+          uploadType: {resumable: false}
+      });
+      
+      const config = {
+        action: 'read',
+        expires: '03-01-2500',
+      }; 
+  
+      await file.save(imageBuffer, (err) => {
+          if (err) {
+              console.error(`Error uploading: ${filename} with message: ${err.message}`);
+              return;
+          }  
+          console.log('Uploaded file');
+      });      
+
+      const imageUrl = await file.getSignedUrl(config);     
+
+      console.log('imageUrl: ' + imageUrl);
+
+      let _time = admin.firestore.FieldValue.serverTimestamp(); 
+
+      await db.collection("cobranzas").doc(cobranzasId).update(            
+        {preview_image: imageUrl.toString(), time_updated_image:_time}
+      ).then((document) => {
+        return document;
+      }).catch((error) => {
+        console.log('Error updating:', error);
+        return error;
+      });        
 
     }); 
-
+   
+    /**
+     * Build Html For Noti
+     */
 
     exports.buildHtmlForNoti = functions.https.onRequest((req, res) => {
       
       console.log("req.path:", req.path); 
-
       const path = req.path.split('/');
       const postId = path[1];        
       
       if (!postId) {
         return res.status(400).send('No se encontro el Id');
-      } 
-
-      console.log("postId:", postId);
+      }       
 
       db.collection('cobranzas').doc(postId).get().then(function (document) {
           
-        if (document.exists) {          
+        if (document.exists) {
+
           let docId = document.id;
           let name = document.data().name;
           let mensaje = document.data().message;
@@ -182,8 +269,12 @@
           let image = document.data().image;
           let title = "Mensaje para " + name;
           const htmlString = htmlFunctions.buildHTMLForPage(title, name, mensaje, contact, image);
+          
           return res.status(200).end(htmlString);
-        }
+        
+        } else {
+          return res.status(404);
+        }        
 
       }).catch(function (error) {
           console.log("Error getting document:", error);
@@ -191,83 +282,31 @@
     
     });
      
-  
-    app.get('/screenshot', async function screenshotHandler(req, res) {
+    /**
+     * Screenshot Test
+     */
+
+    /*app.get('/screenshot', async function screenshotHandler(req, res) {
         const url = req.query.url;
         if (!url) {
         return res.status(400).send(
             'Please provide a URL. Example: ?url=https://google.com');
         }
         const browser = res.locals.browser;
+
         try {
-        const page = await browser.newPage();
-        await page.goto(url, {waitUntil: 'networkidle2'});
-        const buffer = await page.screenshot({fullPage: true});
-        res.type('image/png').send(buffer);
+          const page = await browser.newPage();
+          await page.goto(url, {waitUntil: 'networkidle2'});
+          const buffer = await page.screenshot({fullPage: true});
+          res.type('image/png').send(buffer);
         } catch (e) {
-        res.status(500).send(e.toString());
+          res.status(500).send(e.toString());
         }
         await browser.close();
-    });
-    // Handler that prints the version of headless Chrome being used.
-    app.get('/version', async function versionHandler(req, res) {
-        const browser = res.locals.browser;
-        res.status(200).send(await browser.version());
-        await browser.close();
-    });
-    const opts = {memory: '2GB', timeoutSeconds: 60};
+        return url;
+    });   
+    const opts = {memory: '2GB', timeoutSeconds: 60};    
+    exports.screenshot = functions.runWith(opts).https.onRequest(app);*/
     
-    exports.screenshot = functions.runWith(opts).https.onRequest(app);
-    exports.version = functions.https.onRequest(app);
-    
-    exports.buildHtmlWithContent = functions.https.onRequest((req, res) => {
-        
-        const path = req.path.split('/');
-        const postId = path[2];                
-
-        console.log("postId:", postId);
-
-        db.collection('posts').doc(postId).get().then(function (document) {
-            
-            if (document.exists) {
-                let docId = document.id;
-                let title = document.data().title;
-                const htmlString = buildHtml(post);
-                res.status(200).end(htmlString);
-            }
-
-        }).catch(function (error) {
-            console.log("Error getting document:", error);
-        });
-
-       /* admin.database().ref('/posts').child(postId).once('value').then(snapshot => {
-          const post = snapshot.val();
-          post.id = snapshot.key;
-          const htmlString = buildHtmlWithPost(post);
-          res.status(200).end(htmlString);
-        });*/
-    
-    });
-
-    function buildHtml (title, id) {
-        const string = '<!DOCTYPE html><head>' +
-          '<title>' + title + ' | Example Website</title>' +
-          '<meta property="og:title" content="' + title + '">' +
-          '<meta property="twitter:title" content="' + title + '">' +
-          '<link rel="icon" href="https://example.com/favicon.png">' +
-          '</head><body>' +
-          '<script>window.location="https://example.com/?post=' + id + '";</script>' +
-          '</body></html>';
-        return string;
-      }
-
-
-      app.get("/render", async function(req, res) {
-        console.log("entra render:");
-        const image = await nodeHtmlToImage({
-          html: '<html><body><div>Check out what I just did! #cool</div></body></html>'
-        });
-        res.writeHead(200, { 'Content-Type': 'image/png' });
-        res.end(image, 'binary');
-      });
-      exports.render = functions.https.onRequest(app);
+  
+   
