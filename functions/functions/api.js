ce57a2ee946a176b566fffa49b64c1243001b31c
Jose Vigil <josemanuelvigil@gmail.com>
2020-09-26 22:25:36 -0300

    Nuevo archivo api.js para recibir todos los posts y request de envio.

--- functions/functions/api.js
+++ functions/functions/api.js
@@ -7,9 +7,17 @@
         databaseURL: db_url,
     });  
 
+    //Authorizaation
+    //https://github.com/firebase/functions-samples/tree/master/authorized-https-endpoint
+
     exports.api = functions.https.onRequest( async (req, res) => {
 
-            
+        const tokenId = req.get('Authorization').split('Bearer ')[1];       
+
+        const _recipient = req.body.data.recipient;
+        const _message = req.body.data.message;
+        const _ignore_banned = req.body.data.ignore_banned;
+        const _service_id = req.body.data.service_id;
 
     });
 
